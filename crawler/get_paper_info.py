39dd22aa7cef902032524e37c951debe37b4297d
publicstaticvo <dailyyulun@163.com>
2025-11-04 11:41:24 +0800

    seventhary debug get paper info

--- crawler/get_paper_info.py
+++ crawler/get_paper_info.py
@@ -6,24 +6,23 @@ import logging
 import multiprocessing
 from request_utils import s2api_search_paper
 logging.basicConfig(filename="../logs/info.log", level=logging.INFO)
-fields = ['paperId', 'title', 'abstract', 'venue', 'fieldsOfStudy', "externalIds"]
+fields = ",".join(['title', 'venue', 'publicationVenue', 's2FieldsOfStudy', "externalIds"])
+fin = "../paper2025_with_abs.jsonl"
 foutput = "paper2025_details.jsonl"
 
 
 def request_and_output(paper):
-    response = s2api_search_paper(paper['title'], fields)
-    data = response.get("data", [])
-    for x in data:
-        if paper['title'] not in x['title']: continue
-        venue = x.get("venue", "")
-        if "arxiv" in venue.lower(): continue
-        fields_of_study = x.get("fieldsOfStudy", [])
-        if venue and fields_of_study:
-            paper['venue'] = venue
-            paper['fieldsOfStudy'] = fields_of_study
-            with open(foutput, "a+") as f:
-                f.write(json.dumps(paper) + "\n")
-            return
+    x = s2api_search_paper(arxiv_id=paper['arxiv_url'], fields=fields)
+    venue = x.get("venue", "")
+    publication_venue = x.get("publicationVenue", {})
+    fields_of_study = x.get("s2FieldsOfStudy", [])
+    if (venue or publication_venue) and fields_of_study:
+        external_ids = x.get("externalIds", {})            
+        paper['venue'] = publication_venue if publication_venue else venue
+        paper['external_ids'] = external_ids
+        paper['fields_of_study'] = fields_of_study
+        with open(foutput, "a+", encoding='utf-8') as f:
+            f.write(json.dumps(paper) + "\n")
 
 
 def parallel_main():
@@ -32,7 +31,7 @@ def parallel_main():
     logging.info(f"使用并行进程数: {n_workers}")
     with multiprocessing.Pool(processes=n_workers) as pool:
         pending_results = []
-        with open("../../paper2025_with_abs_and_topic.jsonl", encoding='utf-8') as f:
+        with open(fin, encoding='utf-8') as f:
             for line in f:
                 paper = json.loads(line.strip())
                 if paper['title'].endswith("."): paper['title'] = paper['title'][:-1]
@@ -43,9 +42,36 @@ def parallel_main():
     logging.info(f"Time: {time.time() - t:.4f}")
 
 
+def parallel_main_windows():
+    t = time.time()
+    n_workers = 10
+    logging.info(f"使用并行进程数: {n_workers}")
+    with multiprocessing.Pool(processes=n_workers) as pool:
+        pending_results = []
+        with open(fin, encoding='utf-8') as f:
+            for line in f:
+                paper = json.loads(line.strip())
+                job = pool.apply_async(s2api_search_paper, (paper['title'], paper['arxiv_url'], fields))
+                pending_results.append([job, paper])
+        logging.info("finish pending results")
+        with open(foutput, "w+", encoding='utf-8') as f:
+            for async_result, paper in tqdm.tqdm(pending_results): 
+                x = async_result.get()
+                venue = x.get("venue", "")
+                publication_venue = x.get("publicationVenue", {})
+                fields_of_study = x.get("s2FieldsOfStudy", [])
+                if (venue or publication_venue) and fields_of_study:
+                    external_ids = x.get("externalIds", {})            
+                    paper['venue'] = publication_venue if publication_venue else venue
+                    paper['external_ids'] = external_ids
+                    paper['fields_of_study'] = fields_of_study
+                    f.write(json.dumps(paper) + "\n")
+    logging.info(f"Time: {time.time() - t:.4f}")
+
+
 def main():
     t = time.time()
-    with open("../../paper2025_with_abs_and_topic.jsonl", encoding='utf-8') as f:
+    with open(fin, encoding='utf-8') as f:
         for line in tqdm.tqdm(f):
             paper = json.loads(line.strip())
             if paper['title'].endswith("."): paper['title'] = paper['title'][:-1]
@@ -55,4 +81,5 @@ def main():
 
 
 if __name__ == "__main__":
-    parallel_main()
+    if os.name == "nt": parallel_main_windows()
+    else: parallel_main()
--- crawler/request_utils.py
+++ crawler/request_utils.py
@@ -2,19 +2,33 @@ import time
 import random
 import logging
 import requests
+from typing import Optional
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-#     'x-api-key': 'sk-6aTIdwxxEWiQKciL6fDcB7713169490fAbEcBaC2B0798cEc'
 }
 
 
-def s2api_search_paper(title: str, fields: list[str], max_results: int = 5, retry: int = 3, unlimited_429: bool = True):
+def s2api_search_paper(
+        title: Optional[str] = None, 
+        arxiv_id: Optional[str] = None, 
+        fields: str = "paperId,title", 
+        max_results: int = 5, 
+        retry: int = 3, 
+        unlimited_429: bool = True
+    ):
     """使用Semantic Scholar API搜索"""
+    assert title or arxiv_id, "MUST SPECIFY ONE OF title AND arxiv_id"
+    # arxiv_id first
+    if arxiv_id:
+        url = f"https://api.semanticscholar.org/graph/v1/paper/ARXIV:{arxiv_id}"
+        params = {"fields": fields}
+    else:
+        url = "https://api.semanticscholar.org/graph/v1/paper/search"
+        params = {'query': title, 'limit': max_results, 'fields': fields}  
+    
     while retry != 0:
         try:
-            time.sleep(random.random())
-            url = "https://api.semanticscholar.org/graph/v1/paper/search"
-            params = {'query': title, 'limit': max_results, 'fields': fields}      
+            time.sleep(random.random())                
             response = requests.get(url, headers=headers, params=params, timeout=60)
             response.raise_for_status()
             data = response.json()
@@ -33,8 +47,20 @@ def s2api_search_paper(title: str, fields: list[str], max_results: int = 5, retr
     return {}
 
 
-def s2api_search_paper_single_pass(title: str, fields: list[str], max_results: int = 3):
+def s2api_search_paper_single_pass(
+        title: Optional[str] = None, 
+        arxiv_id: Optional[str] = None, 
+        fields: str = "paperId,title", 
+        max_results: int = 5
+    ):
     """使用Semantic Scholar API搜索，不retry"""
+    assert title or arxiv_id, "MUST SPECIFY ONE OF title AND arxiv_id"
+    if arxiv_id:
+        url = f"https://api.semanticscholar.org/graph/v1/paper/ARXIV:{arxiv_id}"
+        params = {"fields": fields}
+    else:
+        url = "https://api.semanticscholar.org/graph/v1/paper/search"
+        params = {'query': title, 'limit': max_results, 'fields': fields}  
     try:
         time.sleep(random.random())
         url = "https://api.semanticscholar.org/graph/v1/paper/search"
